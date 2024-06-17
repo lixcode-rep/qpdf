d97d8a05cc8077e4ec0422f92be4a6d3eca35915
Vasile <lixcode@vivaldi.net>
2024-06-17 12:48:24 +0300

    Fix compilation on android

--- include/qpdf/Pl_DCT.hh
+++ include/qpdf/Pl_DCT.hh
@@ -25,7 +25,34 @@
 #include <cstddef>
 
 // jpeglib.h must be included after cstddef or else it messes up the definition of size_t.
-#include <jpeglib.h>
+//#include <jpeglib.h>
+typedef unsigned int JDIMENSION;
+typedef enum {
+    JCS_UNKNOWN,            /* error/unspecified */
+    JCS_GRAYSCALE,          /* monochrome */
+    JCS_RGB,                /* red/green/blue as specified by the RGB_RED,
+                             RGB_GREEN, RGB_BLUE, and RGB_PIXELSIZE macros */
+    JCS_YCbCr,              /* Y/Cb/Cr (also known as YUV) */
+    JCS_CMYK,               /* C/M/Y/K */
+    JCS_YCCK,               /* Y/Cb/Cr/K */
+    JCS_EXT_RGB,            /* red/green/blue */
+    JCS_EXT_RGBX,           /* red/green/blue/x */
+    JCS_EXT_BGR,            /* blue/green/red */
+    JCS_EXT_BGRX,           /* blue/green/red/x */
+    JCS_EXT_XBGR,           /* x/blue/green/red */
+    JCS_EXT_XRGB,           /* x/red/green/blue */
+    /* When out_color_space it set to JCS_EXT_RGBX, JCS_EXT_BGRX, JCS_EXT_XBGR,
+     or JCS_EXT_XRGB during decompression, the X byte is undefined, and in
+     order to ensure the best performance, libjpeg-turbo can set that byte to
+     whatever value it wishes.  Use the following colorspace constants to
+     ensure that the X byte is set to 0xFF, so that it can be interpreted as an
+     opaque alpha channel. */
+    JCS_EXT_RGBA,           /* red/green/blue/alpha */
+    JCS_EXT_BGRA,           /* blue/green/red/alpha */
+    JCS_EXT_ABGR,           /* alpha/blue/green/red */
+    JCS_EXT_ARGB,           /* alpha/red/green/blue */
+    JCS_RGB565              /* 5-bit red/6-bit green/5-bit blue */
+} J_COLOR_SPACE;
 
 class QPDF_DLL_CLASS Pl_DCT: public Pipeline
 {
@@ -41,7 +68,7 @@ class QPDF_DLL_CLASS Pl_DCT: public Pipeline
         CompressConfig() = default;
         QPDF_DLL
         virtual ~CompressConfig() = default;
-        virtual void apply(jpeg_compress_struct*) = 0;
+        virtual void apply(void*) = 0;
     };
 
     // Constructor for compressing image data
